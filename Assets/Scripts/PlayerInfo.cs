e47ea53411fbfb4e6cd81e3fa759a3bb687e5077
William Morin <wmsonic@gmail.com>
2021-10-12 20:23:22 -0400

    removed a [serializedField] that was not necessary

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -7,7 +7,6 @@ using UnityEditor.Animations;
 [CreateAssetMenu(fileName="New Character", menuName="Character/Create Character")]
 public class PlayerInfo : ScriptableObject{
 
-	[SerializeField]
 	public GameObject perso;
 
 	public string character_name;
